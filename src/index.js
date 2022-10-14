dfd61f86c97b464a08af75e29c9e778ac796d8a6
Jawid <41361157+infjay@users.noreply.github.com>
2022-10-14 09:45:04 +0200

    Get the list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import FetchWrapper from "./fetch-wrapper.js"
 import { calculateCalories,capitalize } from "./helpers.js"
-
+const snackbar = require('snackbar');
 const API = new FetchWrapper("https://firestore.googleapis.com/v1/projects/mynutrition-753b5/databases/(default)/documents/foods")
 
 const form = document.querySelector("#create-form")
@@ -24,7 +24,7 @@ form.addEventListener("submit", event => {
   { console.log(data);
 
       if(data.error){
-        throw Error ("there was an error with API.Post")
+        snackbar.show("Some data is missing.")
           return;
       }
       foodList.insertAdjacentHTML("beforeend",`
@@ -45,8 +45,34 @@ form.addEventListener("submit", event => {
       carbs.value = "";
       protein.value = "";
       fat.value = "";
+      snackbar.show("Food added successfully.")
   }
   )
 })
+
+
+const init = () => {
+  // TODO: Get the saved entries and list them
+  API.get("").then((data) => data.documents?.forEach((data) => {
+    const fields = data.fields
+    list.insertAdjacentHTML("beforeend",
+    `<li class="card">
+<div>
+  <h3 class="name">${capitalize(fields.name.stringValue)}</h3>
+  <div class="calories">${calculateCalories(
+    fields.carbs.integerValue,
+    fields.protein.integerValue,
+    fields.fat.integerValue)} calories</div>
+  <ul class="macros">
+    <li class="carbs"><div>Carbs</div><div class="value">${fields.carbs.integerValue}g</div></li>
+    <li class="protein"><div>Protein</div><div class="value">${fields.protein.integerValue}g</div></li>
+    <li class="fat"><div>Fat</div><div class="value">${fields.fat.integerValue}g</div></li>
+  </ul>
+</div>
+</li>`)
+  }))
+}
+
+init();
 
 

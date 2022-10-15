d73f42eba8e8f00bf51aaafcfd702dc8168f2191
Jawid <41361157+infjay@users.noreply.github.com>
2022-10-15 12:12:14 +0200

    Chart implemented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import FetchWrapper from "./fetch-wrapper.js"
-import { calculateCalories,capitalize } from "./helpers.js"
+import {calculateCalories,capitalize} from "./helpers.js"
 const snackbar = require('snackbar');
 const API = new FetchWrapper("https://firestore.googleapis.com/v1/projects/mynutrition-753b5/databases/(default)/documents/foods")
+import AppData from "./app-data.js";
+
+import {Chart} from 'chart.js';
 
 const form = document.querySelector("#create-form")
 const name = document.querySelector("#create-name")
@@ -9,68 +12,106 @@ const carbs = document.querySelector("#create-carbs")
 const protein = document.querySelector("#create-protein")
 const fat = document.querySelector("#create-fat")
 const totalCal = document.querySelector("#total-calories")
-const foodList = document.querySelector("#food-list")
+const list = document.querySelector("#food-list")
+
+
+const displayEntry = (name,carbs,protein,fat) => {
+  appData.addFood(carbs,protein,fat)
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<li class="card">
+        <div>
+          <h3 class="name">${capitalize(name)}</h3>
+          <div class="calories">${calculateCalories(
+            carbs,
+            protein,
+            fat
+          )} calories</div>
+          <ul class="macros">
+            <li class="carbs"><div>Carbs</div><div class="value">${carbs}g</div></li>
+            <li class="protein"><div>Protein</div><div class="value">${protein}g</div></li>
+            <li class="fat"><div>Fat</div><div class="value">${fat}g</div></li>
+          </ul>
+        </div>
+      </li>`
+  );
+};
+const appData = new AppData;
+let chartInstance = null;
+const renderChart = () => {
+  chartInstance?.destroy();
+  const context = document.querySelector("#app-chart").getContext("2d");
+
+  chartInstance = new Chart(context, {
+    type: "bar",
+    data: {
+      labels: ["Carbs", "Protein", "Fat"],
+      datasets: [
+        {
+          label: "Macronutrients",
+          data: [appData.getTotalCarbs(), appData.getTotalProtein(), appData.getTotalFat()],
+          backgroundColor: ["#25AEEE", "#FECD52", "#57D269"],
+          borderWidth: 3, // example of other customization
+        },
+      ],
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+  });
+};
+
 
 form.addEventListener("submit", event => {
     event.preventDefault();
     
-    API.post("/",{
+    API.post("",{
     fields: {
     name: { stringValue: name.value },
     carbs: { integerValue: carbs.value },
     protein: { integerValue: protein.value },
     fat: { integerValue: fat.value }
   }}).then(data => 
-  { console.log(data);
+  { 
+    displayEntry(name.value, carbs.value, protein.value, fat.value);
+    snackbar.show("Food added successfully.");
+    renderChart()
 
       if(data.error){
         snackbar.show("Some data is missing.")
           return;
       }
-      foodList.insertAdjacentHTML("beforeend",`
-      <li class="card">
-    <div>
-      <h3 class="name">${capitalize(name.value)}</h3>
-      <div class="calories">${calculateCalories(carbs.value,protein.value,fat.value)} calories</div>
-      <ul class="macros">
-        <li class="carbs"><div>Carbs</div><div class="value">${carbs.value}g</div></li>
-        <li class="protein"><div>Protein</div><div class="value">${protein.value}g</div></li>
-        <li class="fat"><div>Fat</div><div class="value">${fat.value}g</div></li>
-      </ul>
-    </div>
-  </li>`);
-
-  
       name.value = "";
       carbs.value = "";
       protein.value = "";
       fat.value = "";
-      snackbar.show("Food added successfully.")
+      ;
   }
   )
 })
 
 
 const init = () => {
-  // TODO: Get the saved entries and list them
-  API.get("").then((data) => data.documents?.forEach((data) => {
-    const fields = data.fields
-    list.insertAdjacentHTML("beforeend",
-    `<li class="card">
-<div>
-  <h3 class="name">${capitalize(fields.name.stringValue)}</h3>
-  <div class="calories">${calculateCalories(
-    fields.carbs.integerValue,
-    fields.protein.integerValue,
-    fields.fat.integerValue)} calories</div>
-  <ul class="macros">
-    <li class="carbs"><div>Carbs</div><div class="value">${fields.carbs.integerValue}g</div></li>
-    <li class="protein"><div>Protein</div><div class="value">${fields.protein.integerValue}g</div></li>
-    <li class="fat"><div>Fat</div><div class="value">${fields.fat.integerValue}g</div></li>
-  </ul>
-</div>
-</li>`)
-  }))
+  // Get the saved entries and list them
+  API.get("").then((data) => {data.documents?.forEach((data) => {
+
+    const fields = data.fields;
+    // refactor into displayEntry
+    displayEntry(
+      fields.name.stringValue,
+      fields.carbs.integerValue,
+      fields.protein.integerValue,
+      fields.fat.integerValue 
+    );
+  });
+  renderChart();
+})
 }
 
 init();
